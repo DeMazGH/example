61bb3eead65141f73c2f056c972ea642bdb56dad
Denis Maza <demaz@bk.ru>
2022-07-09 18:49:46 +0700

    init

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Люк я твой отец");
+        System.out.println("неееееет");
+        System.out.println("познай силу");
+
+
+    }
+}
