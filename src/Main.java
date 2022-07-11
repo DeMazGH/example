df00d79cf960bb327b7914bd1ed454a3150dd263
Denis Maza <demaz@bk.ru>
2022-07-11 20:53:47 +0700

    third commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Люк я твой отец");
-        System.out.println("неееееет");
+        System.out.println("неееееет!!!!!!!!!!!!!!!!!");
         System.out.println("познай силу");
 
     }
