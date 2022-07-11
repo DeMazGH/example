1f322f78ed1d3eed62d6cb2b945fd4586e105c6f
Denis Maza <demaz@bk.ru>
2022-07-11 20:57:21 +0700

    firstBranchFirstCommit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Люк я твой отец");
+        System.out.println("Люк я твой OTETC");
         System.out.println("неееееет!!!!!!!!!!!!!!!!!");
         System.out.println("познай силу");
 
